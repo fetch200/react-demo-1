b0b7e685e624cdb054e8643191c1db430e27c5c9
EddieMa <machaoxue@yeah.net>
2020-04-30 15:40:08 +0800

    exercise

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import App from './App';
+import './style.css'
 
-let n = 100
+function App() {
+  return (<div className="App">
+    爸爸
+    <Son />
+  </div >)
+}
 
-let App2 = () =>
-  React.createElement('div', null, [n, React.createElement('button', {
-    onClick: () => {
-      n += 1
-      ReactDOM.render(
-        App2(),
-        document.getElementById('root')
-      );
-    }
-  }, '+1')])
+class Son extends React.Component {
+  constructor() {
+    super()
+    this.state = { n: 100 }
+  }
+  add() {
+    this.setState({ n: this.state.n + 1 })
+  }
+  render() {
+    return (<div className="Son">
+      儿子 n
+      <div>{this.state.n}</div>
+      <button onClick={() => this.add()}>+1</button>
+      <Grandson />
+    </div>)
+  }
+}
 
+let Grandson = () => {
+  let [n, setN] = React.useState(100)
+  return (<div className="Grandson">
+    孙子 n
+    <div>{n}</div>
+    <button onClick={() => setN(n + 1)}>+1</button>
+  </div >)
+}
 
 
 ReactDOM.render(
-  App2(),
+  App(),
   document.getElementById('root')
 );
 

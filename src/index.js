3be2cc92c9911ee9df3433741141476fa5e3a6fd
EddieMa <machaoxue@yeah.net>
2020-04-30 12:13:42 +0800

    起手式

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+let n = 100
+
+let App2 = () =>
+  React.createElement('div', null, [n, React.createElement('button', {
+    onClick: () => {
+      n += 1
+      ReactDOM.render(
+        App2(),
+        document.getElementById('root')
+      );
+    }
+  }, '+1')])
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  App2(),
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
